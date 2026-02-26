73752bf5d012544cb64e777169514c4b53c82878
Deeksha Singh <deekshasin1111@gmail.com>
2026-02-26 21:09:47 +0530

    installed multer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 const express = require('express');
-
+const multer = require('multer');
 const app = express();
+
+// multer middleware instance
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+app.use(express.json()); //listens to json requests, a middleware
+
+app.get('/', (req, res) => {
+    res.send("backend is working")
+})
+
+app.post('/create-post', upload.single('image'), (req, res) => {
+    console.log(req.file)
+    console.log(req.body.caption)
+
+    return res.status(200).json({
+        message: "something",
+    })
+})
 
 module.exports = app;
